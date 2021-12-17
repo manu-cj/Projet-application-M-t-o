534e9fea196f2f96f716161dfeab89c2707a8c9c
manucarenjot <carenjotje@gmail.com>
2021-12-17 12:02:01 +0100

    Projet appli meteo

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,13 @@ xhr.open("GET", requestURL);
 xhr.responseType = "json";
 xhr.onload = function () {
     if (xhr.status === 200) {
-        console.log('OK');
+        console.log('Status : OK');
     }
     else if (xhr.status !== 200) {
         alert('Une erreur est survenue : ' +'erreur ' + xhr.status);
         return;
     }
     let response = xhr.response;
-    console.log(response.main.temp_min);
     document.getElementById('ville').innerHTML = response.name + ' ' + response.sys.country;
     document.getElementById('meteo').innerHTML = response.main.temp + '°c';
 }
