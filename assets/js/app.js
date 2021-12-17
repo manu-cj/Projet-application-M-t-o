7eb8aa2b65c1c8a9892937a27128c24a3b9a556f
manucarenjot <carenjotje@gmail.com>
2021-12-17 11:11:21 +0100

    Projet appli meteo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const requestURL = "https://api.openweathermap.org/data/2.5/weather?q=Chimay,be&APPID=200f15a460f2b4b6a04aabd17164ddb0";
+const requestURL = "https://api.openweathermap.org/data/2.5/weather?q=Chimay,be&units=metric&APPID=200f15a460f2b4b6a04aabd17164ddb0";
 let xhr = new XMLHttpRequest();
 xhr.open("GET", requestURL);
 xhr.responseType = "json";
@@ -7,5 +7,13 @@ xhr.onload = function () {
     if (xhr.status === 200) {
         console.log('OK');
     }
+    else if (xhr.status !== 200) {
+        alert('Une erreur est survenue : ' +'erreur ' + xhr.status);
+        return;
+    }
+    let response = xhr.response;
+    console.log(response.main.temp_min);
+    document.getElementById('ville').innerHTML = response.name + ' ' + response.sys.country;
+    document.getElementById('meteo').innerHTML = response.main.temp_max;
 }
 xhr.send()
